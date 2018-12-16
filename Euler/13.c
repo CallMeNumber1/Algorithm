9ccd6f85de68bbaed52875156514f37096e827c6
chongh <304451676@qq.com>
2018-12-16 09:20:58 +0800

    test vscode github

---
+++ Euler/13.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#define MAX 100
+
+int num[MAX] = {1, 0};
+int main() {
+    int n = 100;
+    char str[MAX + 5];
+    for (int i = 0; i < n; i++) {
+        scanf("%s", str);
+        for (int i = 1; i <= num[0]; i++) {
+            num[i] += 
+        }
+    }
+
+    return 0;
+}
